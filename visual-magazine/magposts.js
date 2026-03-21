96ffb7fd129e5b5d65ae97876f0ca42d8c50638b
DrewIt273 <instantdrewIt@gmail.com>
2026-03-21 19:44:30 +0100

    Initializing const variable for keeping references of articles.

--- visual-magazine/magposts.js
+++ visual-magazine/magposts.js
@@ -2,3 +2,6 @@
  * Instance by DrewIt
  */
 
+export const articles = [
+    {t: 'Fruits of Techonology', d: 'Discover how tech has improved our living standards and made our lives more easy.', src: ''},
+]
