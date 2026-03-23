57c25e7f5d650761928df4d0e3a52964c258de83
DrewIt273 <instantdrewIt@gmail.com>
2026-03-23 20:44:41 +0100

    Function to render articles post in 3 containers (prev, now and next) appropriately and function to update a single container.

--- visual-magazine/$script.js
+++ visual-magazine/$script.js
@@ -37,6 +37,23 @@ let currentIndex = 0;
 function getIndexes() {
     const prev = (currentIndex - 1 + articles.length) % articles.length;
     const next = (currentIndex + 1) % articles.length;
-  
-    return { prev, currentIndex, next };
+
+    return {prev, currentIndex, next};
+}
+
+function render() {
+    const {prev, currentIndex: now, next} = getIndexes();
+    const prevEl = posts.find(p => p.hasAttribute('prev'));
+    const nowEl = posts.find(p => p.hasAttribute('now'));
+    const nextEl = posts.find(p => p.hasAttribute('next'));  
+    updatePost(prevEl, articles[prev]);
+    updatePost(nowEl, articles[now]);
+    updatePost(nextEl, articles[next]);
+}
+
+function updatePost(el, data) {
+    const parent = document.querySelector('#con1 va-mag-pic'), desc = parent.querySelector('#mag-description');
+    desc.querySelector('h2').textContent = data.t;
+    desc.querySelector('span').textContent = data.d;
+    el.style.backgroundColor = `url(${data.src})`
 }
