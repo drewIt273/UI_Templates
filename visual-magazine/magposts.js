d1be38d0bd5c1ed7362e54cb541143227d5a05ca
DrewIt273 <instantdrewIt@gmail.com>
2026-03-27 21:24:46 +0100

    Setting 1st 4 articles posts.

--- visual-magazine/magposts.js
+++ visual-magazine/magposts.js
@@ -3,5 +3,8 @@
  */
 
 const articles = [
-    {t: 'Fruits of Techonology', d: 'Discover how tech has improved our living standards and made our lives more easy.', src: ''},
+    {t: 'Sunset Tree in Nature', d: 'Discover the fascinating secrets of plants that benfits the whole earth.', img: 'Sunset Light Tree in Nature.jpg'},
+    {t: 'Gaming and Entertainment', d: 'Why do video games became so popular at a point where it is now an important industry?', img: 'gaming.jpg'},
+    {t: 'Futuristic Architecture', d: 'Discover how Architects plan our future buildings and cities', img: 'futuristic architecture.jpg'},
+    {t: 'Web Programming', d: 'How to become a ready-to-employ web developer in less than 10 weeks?', img: 'Developer programmer.jpg'}
 ]
