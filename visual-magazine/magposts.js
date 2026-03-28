a9d2d089a06bfcdf1cbe1aacab4935c8687cc4cf
DrewIt273 <instantdrewIt@gmail.com>
2026-03-28 14:37:53 +0100

    Adding posts.

--- visual-magazine/magposts.js
+++ visual-magazine/magposts.js
@@ -6,5 +6,10 @@ const articles = [
     {t: 'Sunset Tree in Nature', d: 'Discover the fascinating secrets of plants that benfits the whole earth.', img: 'Sunset Light Tree in Nature.jpg'},
     {t: 'Gaming and Entertainment', d: 'Why do video games became so popular at a point where it is now an important industry?', img: 'gaming.jpg'},
     {t: 'Futuristic Architecture', d: 'Discover how Architects plan our future buildings and cities', img: 'futuristic architecture.jpg'},
-    {t: 'Web Programming', d: 'How to become a ready-to-employ web developer in less than 10 weeks?', img: 'Developer programmer.jpg'}
+    {t: 'Web Programming', d: 'How to become a ready-to-employ web developer in less than 10 weeks?', img: 'Developer programmer.jpg'},
+    {t: 'Software Programming', d: 'How do programmers and software engineers improve our lives everyday?', img: 'cpp lang.jpg'},
+    {t: 'Fantasy in films', d: 'What makes sci-fi films feel amazing is the cinematics and imaginary environments in which everything goes on. Discover why.', img: 'Dreamy Fantasy Landscapes.jpg'},
+    {t: 'Hand signs in animes', d: 'Why do many animes include characters executing mysterious hand signs?', img: 'Hand signs in animes.jpg'},
+    {t: 'Anime Female', d: 'Why do anime studios make their female characters in animes look so attractive?', img: 'cha hae in.jpg'},
+    {t: 'Universe of Animes', d: 'Discover why most peoples like watching animes and why they are worth watching.', img: 'anime universe.jpg'},
 ]
