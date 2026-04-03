d8cfb4eed209151a8b3779c57e6ec27606b333cf
DrewIt273 <instantdrewIt@gmail.com>
2026-04-03 14:26:39 +0100

    Adding 'i' property to articles for introduction text.

--- visual-magazine/magposts.js
+++ visual-magazine/magposts.js
@@ -6,6 +6,7 @@ const articles = [{
     t: 'Sunset Tree in Nature',
     d: 'Discover the fascinating secrets of plants that benfits the whole earth.',
     img: 'Sunset Light Tree in Nature.jpg',
+    i: '',
 }, {
     t: 'Gaming and Entertainment',
     d: 'Why do video games became so popular at a point where it is now an important industry?',
@@ -15,60 +16,75 @@ const articles = [{
     t: 'Futuristic Architecture',
     d: 'Discover how Architects plan our future buildings and cities',
     img: 'futuristic architecture.jpg',
+    i: '',
 }, {
     t: 'Web Programming',
     d: 'How to become a ready-to-employ web developer in less than 10 weeks?',
     img: 'Developer programmer.jpg',
+    i: '',
 }, {
     t: 'Software Programming',
     d: 'How do programmers and software engineers improve our lives everyday?',
     img: 'cpp lang.jpg',
+    i: '',
 }, {
     t: 'Fantasy in films',
     d: 'What makes sci-fi films feel amazing is the cinematics and imaginary environments in which everything goes on. Discover why.',
     img: 'Dreamy Fantasy Landscapes.jpg',
+    i: '',
 }, {
     t: 'Hand signs in animes',
     d: 'Why do many animes include characters executing mysterious hand signs?',
     img: 'Hand signs in animes.jpg',
+    i: '',
 }, {
     t: 'Anime Female',
     d: 'Why do anime studios make their female characters in animes look so attractive?',
     img: 'cha hae in.jpg',
+    i: '',
 }, {
     t: 'Universe of Animes',
     d: 'Discover why most peoples like watching animes and why they are worth watching.',
     img: 'anime universe.jpg',
+    i: '',
 }, {
     t: 'Virtual Reality Worlds',
     d: 'Why VR is becoming the next frontier for gaming and social interaction.',
     img: 'vr-headset.jpg',
+    i: '',
 }, {
     t: 'Music Evolution',
     d: 'From classical to digital beats, how soundscapes change society.',
     img: 'when-music-matches-the-mood.jpg',
+    i: '',
 }, {
     t: 'Digital Art Evolution',
     d: 'How tools like Procreate and Blender empower new artists.',
     img: 'music-lover.jpg',
+    i: '',
 }, {
     t: 'Human Brain Wonders',
     d: 'Exploring creativity, memory, and consciousness. Your Brain is much more complex than you think.',
     img: '',
+    i: '',
 }, {
     t: 'Connected Devices',
     d: 'Oning your tv using your phone or editing a text document you uploaded from your phone on your computer, discover how it works.',
     img: 'connected-device.jpg',
+    i: '',
 }, {
     t: 'City Dreams',
     d: 'How urban landscapes inspire creativity and ambition.',
     img: 'drm.jpg',
+    i: '',
 }, {
     t: 'Journeys by Rail',
     d: 'Why train travel remains one of the most inspiring ways to see the world.',
     img: 'train transport.jpg',
+    i: '',
 }, {
     t: 'Streetwear & Identity',
     d: 'Why fashion is a powerful form of self-expression in modern youth culture.',
     img: 'Sasuke.jpg',
+    i: '',
 }, ]
