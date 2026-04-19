39dfd16c75b7e82b21bf742d533481aeb77e1e12
DrewIt273 <instantdrewIt@gmail.com>
2026-04-19 20:17:34 +0100

    Defining catalogues structure with first 5 catalogues.

--- visual-magazine/catalogues.js
+++ visual-magazine/catalogues.js
@@ -3,7 +3,28 @@
  */
 
 const catalogues = [{
-    t: 'Travel',
-    d: '',
-    img: 'train transport.jpg',
+    t: 'The Future Unfolding',
+    d: 'Exploring the ideas, technologies, and breakthroughs shaping what comes next.',
+    img: '',
+    tgs: ['Technology', 'Science', 'Business'],
+}, {
+    t: 'The World in Transition',
+    d: 'A look at the shifts redefining how we live, work, and connect.',
+    img: '',
+    tgs: ['World', 'Business', 'Lifestyle'],
+}, {
+    t: 'Beyind the Screen',
+    d: 'Stories, worlds, and creativity that live through screens and beyond.',
+    img: '',
+    tgs: ['Entertainment', 'Culture', 'Creativity'],
+}, {
+    t: 'New & Noteworthy',
+    d: 'Fresh perspectives, emerging trends, and stories worth your attention.',
+    img: '',
+    tgs: ['Technology', 'Entertainment', 'Culture'],
+}, {
+    t: 'Aesthetic Realms',
+    d: 'A collection of visuals and designs that capture beauty and imagination.',
+    img: '',
+    tgs: ['Creativity', 'Lifestyle', 'Culture']
 }]
